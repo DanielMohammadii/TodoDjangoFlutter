da5f424f62c9f217eda8bc6b0107d972e56d017c
daniel <road2product@gmail.com>
2022-11-06 15:29:49 +0000

    'all'

---
+++ api/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Todo(models.Model):
+
+    Title = models.CharField(max_length=200, blank=100)
+    Desc = models.TextField(blank=False)
+    Date = models.DateField(auto_now_add=True)
+    completed = models.BooleanField(default=False, blank=False)
+
+    def __str__(self):
+        return self.Title
---
+++ api/views.py
@@ -0,0 +1,22 @@
+from rest_framework import generics
+from .models import Todo 
+from .serializers import *
+
+
+#Crud Operations
+class ListTodo(generics.ListAPIView):     #Read
+    queryset =  Todo.objects.all()
+    serializer_class = TodoSerializer
+
+class DetailTodo(generics.RetrieveDestroyAPIView):   #Update
+    queryset =  Todo.objects.all()
+    serializer_class = TodoSerializer
+
+class CreateTodo(generics.CreateAPIView):     #Create
+    queryset =  Todo.objects.all()
+    serializer_class = TodoSerializer
+
+
+class DeleteTodo(generics.DestroyAPIView):   #Delete
+    queryset =  Todo.objects.all()
+    serializer_class = TodoSerializer
