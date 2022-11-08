58bf6373525162a7d4495c65547e5cf2c1a05d57
daniel <road2product@gmail.com>
2022-11-08 07:45:05 +0000

    change update view

--- api/views.py
+++ api/views.py
@@ -1,22 +1,24 @@
 from rest_framework import generics
-from .models import Todo 
+from .models import Todo
 from .serializers import *
 
 
-#Crud Operations
-class ListTodo(generics.ListAPIView):     #Read
-    queryset =  Todo.objects.all()
-    serializer_class = TodoSerializer
-
-class DetailTodo(generics.RetrieveDestroyAPIView):   #Update
-    queryset =  Todo.objects.all()
-    serializer_class = TodoSerializer
-
-class CreateTodo(generics.CreateAPIView):     #Create
-    queryset =  Todo.objects.all()
+# Crud Operations
+class ListTodo(generics.ListAPIView):  # Read
+    queryset = Todo.objects.all()
     serializer_class = TodoSerializer
 
 
-class DeleteTodo(generics.DestroyAPIView):   #Delete
-    queryset =  Todo.objects.all()
+class DetailTodo(generics.RetrieveUpdateAPIView):  # Update
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+
+class CreateTodo(generics.CreateAPIView):  # Create
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+
+class DeleteTodo(generics.DestroyAPIView):  # Delete
+    queryset = Todo.objects.all()
     serializer_class = TodoSerializer
