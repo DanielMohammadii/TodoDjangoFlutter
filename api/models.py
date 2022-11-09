e30360485bdce9d2d20f9ddc2fef7779173cf887
daniel <road2product@gmail.com>
2022-11-09 10:19:04 +0000

    change the desc

--- api/models.py
+++ api/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Todo(models.Model):
 
     Title = models.CharField(max_length=200, blank=100)
-    Desc = models.TextField(blank=False)
+    Desc = models.TextField(blank=True)
     Date = models.DateField(auto_now_add=True)
     completed = models.BooleanField(default=False, blank=False)
 
